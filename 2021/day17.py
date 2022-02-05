d8fc9c48ac090021d3138c25cabab227f6380813
s2396041 <s.deboer-1@student.utwente.nl>
2022-02-05 19:19:00 +0100

    cleaned up code

--- 2021/day17.py
+++ 2021/day17.py
@@ -2,7 +2,6 @@ with open("input-17.txt", "r") as f:
     x0, y0 = [a[2:].split("..") for a in f.readline().strip()[13:].split(", ")]
     x_min, x_max = int(x0[0]), int(x0[1])
     y_min, y_max = int(y0[0]), int(y0[1])
-    print(x_min, x_max, y_min, y_max)
 
 
 def in_target_area(x, y):
@@ -31,7 +30,6 @@ def probe(x_velocity, y_velocity, find_y=False):
         if x > x_max or y_min > y:
             break
         if find_y:
-            print(x, y)
             if y > max_y:
                 max_y = y
             if in_target_area(x, y):
@@ -39,19 +37,18 @@ def probe(x_velocity, y_velocity, find_y=False):
         else:
             z = in_target_area(x, y)
             if z:
-                # print("HIT (%s, %s)" % (x, y))
                 probes.append([i_x, i_y])
                 if y_velocity < 0:
                     break
 
 
 if __name__ == "__main__":
-    for x in range(-300, 300):
-        for y in range(-300, 300):
+    r = 150
+    for x in range(-r, r):
+        for y in range(-r, r):
             if x or y:
                 probe(x, y)
-    print(probes)
     for p in probes:
         probe(p[0], p[1], find_y=True)
-    print(max_y)
-    print(len(probes))
+    print("Part 1: %s" % max_y)
+    print("Part 2: %s" % len(probes))
